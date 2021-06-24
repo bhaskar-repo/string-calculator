61ba72fa9f59a5cc6e5fbb5ee6a05ae906738a7c
bhaskarpawar123 <bhaskar.pawar@kylas.io>
2021-06-24 15:06:04 +0530

    initial commit

---
+++ src/test/java/com/tdd/kata/string/calculator/StringCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd.kata.string.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
