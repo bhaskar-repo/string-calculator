9c4c711a115a4bf8c55d0066647f8089a6b372f1
bhaskarpawar123 <bhaskar.pawar@kylas.io>
2021-06-24 17:29:26 +0530

    failing test case(1) empty string

---
+++ src/main/java/com/tdd/kata/string/calculator/StringCalculator.java
@@ -0,0 +1,11 @@
+package com.tdd.kata.string.calculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StringCalculator {
+
+  public int add(String inputString) {
+    return -1;
+  }
+}
---
+++ src/test/java/com/tdd/kata/string/calculator/StringCalculatorTest.java
@@ -0,0 +1,24 @@
+package com.tdd.kata.string.calculator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class StringCalculatorTest {
+
+  @InjectMocks
+  private StringCalculator stringCalculator;
+
+  @Test
+  public void givenAnEmptyString_shouldReturn0() {
+    //given
+    String emptyString = "";
+    //when & //then
+    assertThat(stringCalculator.add(emptyString)).isZero();
+  }
+
+}
