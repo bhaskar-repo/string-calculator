cc1cfd104eed17d230ba378d51559b808a388192
bhaskarpawar123 <bhaskar.pawar@kylas.io>
2021-06-24 18:01:44 +0530

    passed test case(6) accepting delimiter other than comma and new line

--- src/main/java/com/tdd/kata/string/calculator/StringCalculator.java
+++ src/main/java/com/tdd/kata/string/calculator/StringCalculator.java
@@ -13,7 +13,15 @@ public class StringCalculator {
     if (inputString.length() == 1) {
       return Integer.parseInt(inputString);
     }
-    return Arrays.stream(inputString.split(",|\n"))
+    String[] inputNumbers = inputString.split(",|\n");
+    if (inputString.startsWith("//")) {
+      StringBuilder stringBuilder = new StringBuilder();
+      String[] delimiters = inputString.split("\n");
+      String regex = stringBuilder.append(delimiters[0].replace("//", "")).toString();
+      inputNumbers = delimiters[1].split(regex);
+    }
+    return Arrays.stream(inputNumbers)
+        .filter(stringNumber -> !stringNumber.isEmpty())
         .mapToInt(Integer::parseInt)
         .sum();
   }
